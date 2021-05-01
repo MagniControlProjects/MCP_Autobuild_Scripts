fb7910f73d2897b49b7924d20caa1c2de007519c
george Linsdell <linsdell.george@gmail.com>
2021-05-01 22:37:24 +0100

    JAB-1 Create Main Entry helper script to store access to core tools.

---
+++ main.groovy
@@ -0,0 +1,37 @@
+/*
+/   @author: George Linsdell
+/   @date:01/05/2021
+/   Contains a helper object as an initial test to see how objects work. 
+/   Object will grow to support minimal requirements in jobs.
+/   The assumption is made that jenkins Environment variables are present.
+/   
+*/
+
+class Helper (){
+    
+    static void main (String [] args){
+        //In theory, args may be used to overwrite
+        this.args = args
+        
+        this.initialize()
+    }
+    
+    static void main (){
+        //Overloading the main method to allow non arguments to be passed for initial testing.
+        this.args = {}
+        this.initialize()
+    }
+    
+    static void initialize (){
+        this.Workspace = "${WORKSPACE}"
+        echo `Workspace is ${this.Workspace}`
+        this.BuildId = "${BUILD_ID}"
+        this.BuildName = "${BUILD_NAME}"
+        
+        echo `Helper Initialized for Build Name ${this.BuildName}-${this.BuildId}`
+    }
+    
+    static void test (){
+        echo "test method Entered"
+    }
+}
