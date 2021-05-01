e6afb72f8b5eb4627c3aa1250b7dc885cf850644
george Linsdell <linsdell.george@gmail.com>
2021-05-01 23:18:34 +0100

    JAB-1: multi stage jobs

--- compile/main.groovy
+++ compile/main.groovy
@@ -2,50 +2,52 @@ pipeline{
     agent {
         label "${AGENT_LABEL}"
     }
-    stage ("Initialize Job"){
-        steps{
-            script{
-                
-                COMMON_UTILS_LOAD_LOCATION = "${WORKSPACE}/autobuild/CommonUtils/main.groovy"
-                echo ("CommonUtils at ${COMMON_UTILS_LOAD_LOCATION}")
-                Helper1 = load (COMMON_UTILS_LOAD_LOCATION).GenerateHelper()
-                
+    stages{
+        stage ("Initialize Job"){
+            steps{
+                script{
+                    
+                    COMMON_UTILS_LOAD_LOCATION = "${WORKSPACE}/autobuild/CommonUtils/main.groovy"
+                    echo ("CommonUtils at ${COMMON_UTILS_LOAD_LOCATION}")
+                    Helper1 = load (COMMON_UTILS_LOAD_LOCATION).GenerateHelper()
+                    
+                }
             }
         }
-    }
-    stage("Checkout Dependencies"){
-        steps{
-            script{
-                echo "Do Stuff"
-                Helper.test()
+        stage("Checkout Dependencies"){
+            steps{
+                script{
+                    echo "Do Stuff"
+                    Helper.test()
+                }
             }
         }
-    }
-    stage("Setup Build Folder"){
-        steps{
-            script{
-                echo "Do Stuff"
+        stage("Setup Build Folder"){
+            steps{
+                script{
+                    echo "Do Stuff"
+                }
             }
         }
-    }
-    stage("Generate Version Info"){
-        steps{
-            script{
-                echo "Do Stuff"
+        stage("Generate Version Info"){
+            steps{
+                script{
+                    echo "Do Stuff"
+                }
             }
         }
-    }
-    stage("Do Building"){
-        steps{
-            script{
-                echo "Do Stuff"
+        stage("Do Building"){
+            steps{
+                script{
+                    echo "Do Stuff"
+                }
             }
         }
-    }
-    stage("Store Artifacts"){
-        steps{
-            script{
-                echo "Do Stuff"
+        stage("Store Artifacts"){
+            steps{
+                script{
+                    echo "Do Stuff"
+                }
             }
         }
     }
