dae07665e1567d6fb6cfe4554378dcb5e4a42839
george Linsdell <linsdell.george@gmail.com>
2021-05-04 00:44:43 +0100

    JAB-1: Commit Licensing

--- CommonUtils/helper.groovy
+++ CommonUtils/helper.groovy
@@ -1,12 +1,24 @@
-/*
- *   @author: George Linsdell
- *   @date:03/05/2021
- *   Contains a helper object as an initial test to see how objects work. 
- *   Object will grow to support minimal requirements in jobs.
- *   The assumption is made that jenkins Environment variables are present.
- *   
- *  apparently << = append to list.
+/* Copyright 2021 George Linsdell
  * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+ * permit persons to whom the Software is furnished to do so, subject to the following 
+ * conditions:
+ * The above copyright notice and this permission notice shall be included in all copies 
+ * or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * @date:03/05/2021
+ * Contains a helper object as an initial test to see how objects work. 
+ * Object will grow to support minimal requirements in jobs.
+ * The assumption is made that jenkins Environment variables are present..
  */
 
 def printTrace (List tracelist){
